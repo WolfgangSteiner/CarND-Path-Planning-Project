a0819793a07b8861448782f09d2287ccda9772a4
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-08-14 13:41:18 +0200

    increase horizon time

--- src/VehicleState.h
+++ src/VehicleState.h
@@ -26,7 +26,7 @@ protected:
   double VelocityCorrection(int aLaneNumber) const;
 
 protected:
-  double mHorizonTime{4.0};
+  double mHorizonTime{8.0};
   double mMaxVelocity{49.0 * 0.447};
   double mCostDeltaT{0.1};
 };
